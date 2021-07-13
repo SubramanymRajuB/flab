6b02d85ad4bde5cb66364aa53a37ca794862d1c3
balarsu <subramanyam.balaraju@lamresearch.com>
2021-07-13 12:06:01 +0530

    - Added custom platform-specific code demo

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -6,8 +6,51 @@ import Flutter
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+  ) -> Bool
+  {
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    
+    let simpleMsgChannel = FlutterMethodChannel(name: "flutter.native/simple_msg",
+                                              binaryMessenger: controller.binaryMessenger)
+    simpleMsgChannel.setMethodCallHandler({
+      [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+      // Note: this method is invoked on the UI thread.
+      guard call.method == "simpleMsgFromNative" else {
+        result(FlutterMethodNotImplemented)
+        return
+      }
+      self?.simpleMsgFromNative(result: result)
+    })
+
+    let batteryChannel = FlutterMethodChannel(name: "flutter.native/battery",
+                                              binaryMessenger: controller.binaryMessenger)
+    batteryChannel.setMethodCallHandler({
+      [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+      // Note: this method is invoked on the UI thread.
+      guard call.method == "getBatteryLevel" else {
+        result(FlutterMethodNotImplemented)
+        return
+      }
+      self?.receiveBatteryLevel(result: result)
+    })
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  private func simpleMsgFromNative(result: FlutterResult) {
+      result("Hello From Native IOS Code");
+  }
+
+  private func receiveBatteryLevel(result: FlutterResult) {
+    let device = UIDevice.current
+    device.isBatteryMonitoringEnabled = true
+    if device.batteryState == UIDevice.BatteryState.unknown {
+      result(FlutterError(code: "UNAVAILABLE",
+                          message: "Battery info unavailable",
+                          details: nil))
+    } else {
+      result(Int(device.batteryLevel * 100))
+    }
   }
 }
---
+++ lib/native/simple_msg.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter/services.dart';
+
+
+class SimplePlatformMsg extends StatefulWidget {
+  SimplePlatformMsg({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _SimplePlatformMsgState createState() => new _SimplePlatformMsgState();
+}
+class _SimplePlatformMsgState extends State<SimplePlatformMsg> {
+  static const msgChannel = const MethodChannel('flutter.native/simple_msg');
+  static const batteryChannel = const MethodChannel('flutter.native/battery');
+
+  String _responseFromNativeCode = 'Waiting for Response...';
+  Future<void> _getNativeMsg() async {
+    String response = "";
+    try {
+      final String result = await msgChannel.invokeMethod('simpleMsgFromNative');
+      response = result;
+    } on PlatformException catch (e) {
+      response = "Failed to Invoke: '${e.message}'.";
+    }
+    setState(() {
+      _responseFromNativeCode = response;
+    });
+  }
+
+  // Get battery level.
+  String _batteryLevel = 'Waiting for Battery Status...';
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int result = await batteryChannel.invokeMethod('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+        appBar: AppBar(
+        backgroundColor: Colors.lightBlue[900],
+        elevation: 0,
+        title: Text("Native API Acess Demo", style: TextStyle(color: Colors.white),),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white70,),
+          onPressed: (){
+            Navigator.of(context, rootNavigator: true).pop(context);
+          },
+          ),
+      ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                child: Text('Simple Message from Native'),
+                onPressed: _getNativeMsg,
+              ),
+              Text(_responseFromNativeCode),
+              SizedBox(height: 10),
+              ElevatedButton(
+                child: Text('Get Battery Status'),
+                onPressed: _getBatteryLevel,
+              ),
+              Text(_batteryLevel),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
