e5e15531c428dc97b7785fe530b4a6721a0f3a0a
balarsu <subramanyam.balaraju@lamresearch.com>
2021-07-17 14:21:47 +0530

    - Added  app & widget life cycle demo

---
+++ lib/lifecycle/pages/home_page.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+
+import 'new_page.dart';
+
+class IntializeTitle extends StatefulWidget {
+    @override
+  _IntializeTitleState createState() => _IntializeTitleState();
+}
+
+class _IntializeTitleState extends State<IntializeTitle> {
+  String title = 'First Page Old Title';
+  VoidCallback onPressed;
+  void updateTitle(){
+    setState(() {
+      title='First Page New Title';
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FirstPage(title: title, onPressed: updateTitle,),
+    );
+  }
+}
+
+class FirstPage extends StatefulWidget {
+  FirstPage({Key key, this.title, this.onPressed}) : super(key: key);
+
+  final String title;
+  final VoidCallback onPressed; // For understanding didUpdateWidget
+
+//Widget Life cycles
+  @override
+  _FirstPageState createState() {
+    print("1: Create State");
+    return _FirstPageState();
+  }
+}
+
+class _FirstPageState extends State<FirstPage> {
+
+//Widget Life cycles
+  @override
+  void initState() {
+    print("2: initState");
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    print("3: didChangeDependencies");
+    super.didChangeDependencies();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    print("4: build");
+    
+    int count=0;
+    void _increment(){
+      setState(() {
+           print("6: setState");
+           count++;
+          });
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'counter value is: $count',
+                ),
+                SizedBox(height: 8.0,),
+                Text(
+                  'Orientation: $orientation',
+                ),
+                SizedBox(height: 8.0,),
+                ElevatedButton(
+                  child: Text('DidUpdateWidget'),
+                  onPressed: widget.onPressed,
+                ),
+                SizedBox(height: 8.0,),
+                ElevatedButton(
+                  child: Text('Navigate to new route'),
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (context) => NewPage(),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+      floatingActionButton:  FloatingActionButton(
+        child: Icon(Icons.plus_one),
+        onPressed: (){
+          _increment();
+        }
+      ),
+    );
+  }
+
+  @override
+  void didUpdateWidget(covariant FirstPage oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print("5: didUpdateWidget");
+    if(this.widget.title!=oldWidget.title){
+      print("Title Changed");
+    }
+  }
+
+  @override
+  void deactivate() {
+    print("7: deactivate");
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print("8: dispose");
+    super.dispose();
+  }
+
+}
+
---
+++ lib/lifecycle/pages/new_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NewPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Page'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white70,),
+          onPressed: (){
+            Navigator.of(context, rootNavigator: true).pop(context);
+          },
+          )
+      ),
+    );
+  }
+}
+
