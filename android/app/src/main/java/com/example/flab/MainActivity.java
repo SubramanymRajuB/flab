c5f9daf92cbbfdf2344fb4e47de67a403fd8fc49
balarsu <subramanyam.balaraju@lamresearch.com>
2021-07-20 10:19:14 +0530

    - Migrated flutter kotlin to java using  "flutter create -a java .", before need to delete android total directory

---
+++ android/app/src/main/java/com/example/flab/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.flab;
+
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+
+public class MainActivity extends FlutterActivity {
+    private static final String MSG_CHANNEL = "flutter.native/simple_msg";
+    private static final String BATTERY_CHANNEL = "flutter.native/battery";
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(),MSG_CHANNEL)
+                .setMethodCallHandler(
+                        ((call, result) -> {
+                            if(call.method.equals("simpleMsgFromNative")){
+                                result.success(helloFromNativeCode());
+                            }
+                            else {
+                                result.notImplemented();
+                            }
+                        })
+                );
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), BATTERY_CHANNEL)
+                .setMethodCallHandler(
+                        ((call, result) -> {
+                            if (call.method.equals("getBatteryLevel")) {
+                                int batteryLevel = getBatteryLevel();
+
+                                if (batteryLevel != -1) {
+                                    result.success(batteryLevel);
+                                } else {
+                                    result.error("UNAVAILABLE", "Battery level not available.", null);
+                                }
+                            } else {
+                                result.notImplemented();
+                            }
+
+                        })
+                );
+    }
+
+    private String helloFromNativeCode() {
+        return "Hello from Native Android Code";
+    }
+
+    private int getBatteryLevel() {
+        int batteryLevel = -1;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+        } else {
+            Intent intent = new ContextWrapper(getApplicationContext()).
+                    registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+            batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) /
+                    intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        }
+
+        return batteryLevel;
+    }
+}
