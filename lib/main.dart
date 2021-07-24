43f8cdf25747ee46b0b60555467dfdfa02d96c1c
balarsu <subramanyam.balaraju@lamresearch.com>
2021-07-24 19:29:30 +0530

    - Default counter demo added

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flab/startup/demo.dart';
 import 'package:flab/startup/flab_demos.dart';
 import 'package:flutter/material.dart';
 
+import 'startup/default_counter.dart';
+
 void main() {
   // runApp(Demo());
-  runApp(Welcome());
+  runApp(DefaultCounter());
 }
---
+++ lib/startup/default_counter.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+
+class DefaultCounter extends StatefulWidget {
+  @override
+  DefaultCounterState createState() => DefaultCounterState();
+}
+
+class DefaultCounterState extends State<DefaultCounter> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Default Counter"),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          )),
+    );
+  }
+}
